ea6958867d94e6171b8da2fa3b133191d8b8af31
Anjali04040 <anjali04042018@gmail.com>
2024-07-23 10:12:54 +0530

    otp verification

--- backend/main.js
+++ backend/main.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("./config/db.js")
 const authRouter = require("./routes/authRoutes.js")
 const otpRouter = require("./routes/otpRoute.js");
+const folderRouter = require("./routes/folderRoutes.js");
 const express = require("express");
 const cors = require("cors");
 const verifyToken = require("./middlewares/verifyToken.js");
@@ -17,6 +18,7 @@ app.use("/api/v1/auth",authRouter);
 
 app.use(verifyToken);
 app.use("/api/v1/otp",otpRouter)
+app.use("/api/v1/folder",folderRouter);
 
 app.listen(process.env.PORT, () =>{
     console.log("-------------App Listen on port " + process.env.PORT + " ---------------")
